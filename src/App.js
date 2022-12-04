a997cac615009dde764558eb4c6935816714fd8d
Salman Hossain <salmann.hossain@gmail.com>
2022-12-04 16:44:58 +0600

    component2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes'
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes:DISHES
+    }
+     
+  }
+
+  render()  {
   return (
     <div >
         <Navbar dark color='primary'>
@@ -13,9 +26,9 @@ function App() {
           </NavbarBrand>
         </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
     </div>
-  );
+  );}
 }
 
 export default App;
